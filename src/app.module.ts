d0a63413390d0f6aa386f91524baa1fbd51bd804
Franklin Jorge <franklin-jorge@hotmail.com>
2024-08-12 22:21:13 -0400

    chore: Update JWT keys in env.ts and add JWT module to AuthModule
    REMOVING UNUSED FILES#

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,25 @@ import PrismaService from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticationController } from './controllers/authenticate.controller'
+import { CreationQuestionController } from './controllers/create-question.controller'
+import { FetchQuestionListController } from './controllers/fetch-question-list.controller'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true
-    })
+    }),
+    AuthModule
   ],
-  controllers: [CreateAccountController],
+  controllers: [
+    CreateAccountController,
+    AuthenticationController,
+    CreationQuestionController,
+    FetchQuestionListController
+  ],
   providers: [PrismaService]
 })
 export class AppModule {}
