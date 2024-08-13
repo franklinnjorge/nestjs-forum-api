cf41d8f4f3824abed3dcc898ab181b5ef9083d9c
Franklin Jorge <franklin-jorge@hotmail.com>
2024-08-13 17:28:33 -0400

    feat: Add unit test for creating a new account

---
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -0,0 +1,10 @@
+test('1 plus 1', () => {
+  beforeEach({
+    //preciso iniciar algo
+  })
+
+  describe('should create a new account', () => {
+    it('should')
+  })
+  expect(1 + 1).equal(2)
+})
